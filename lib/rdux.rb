c340d81775502aadc7cf03fa273b613b6837d186
himn1 <hello@artofcode.co>
2023-06-12 14:25:24 +0200

    Rubocop is green

--- lib/rdux.rb
+++ lib/rdux.rb
@@ -4,14 +4,34 @@ require 'rdux/engine'
 require 'rdux/result'
 
 module Rdux
-  def self.dispatch(action_name, payload, opts = {})
-    action = Action.new(name: action_name, up_payload: payload)
-    res = action.call(opts)
-    res = action.up(opts) if res.nil?
-    action.down_payload = res.down_payload
-    res.down_payload = action.down_payload
-    action.save! if res.ok
-    res.action = action
-    res
+  class << self
+    def dispatch(action_name, payload, opts = {})
+      action = Action.new(name: action_name, up_payload: payload)
+      call_call_meth_on_action(action, opts)
+    end
+
+    private
+
+    def call_call_meth_on_action(action, opts)
+      res = action.call(opts)
+      return call_up_meth_on_action(action, opts) if res.nil?
+
+      res.resp = res.down_payload.deep_stringify_keys
+      res.down_payload = nil
+      assign_and_persist(res, action)
+    end
+
+    def call_up_meth_on_action(action, opts)
+      res = action.up(opts)
+      res.down_payload.deep_stringify_keys!
+      action.down_payload = res.down_payload
+      assign_and_persist(res, action)
+    end
+
+    def assign_and_persist(res, action)
+      action.save! if res.ok
+      res.action = action
+      res
+    end
   end
 end
