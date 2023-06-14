3384b27afcb2b41f0ced664e0aa5363ba2574eeb
himn1 <hello@artofcode.co>
2023-06-14 15:07:53 +0200

    refactoring

--- lib/rdux.rb
+++ lib/rdux.rb
@@ -2,6 +2,7 @@
 
 require 'rdux/engine'
 require 'rdux/result'
+require 'rdux/sanitize'
 
 module Rdux
   class << self
@@ -30,29 +31,11 @@ module Rdux
 
     def assign_and_persist(res, action)
       if res.ok
-        action.up_payload = filter_payload(action.up_payload)
+        action.up_payload = Sanitize.call(action.up_payload)
         action.save!
       end
       res.action = action
       res
-    end
-
-    def filter_payload(payload)
-      filtered_payload = payload.deep_dup # Create a duplicate to avoid modifying the original params
-      Rails.application.config.filter_parameters.each do |filter_param|
-        filter_recursive(filtered_payload, filter_param)
-      end
-      filtered_payload
-    end
-
-    def filter_recursive(payload, filter_param)
-      payload.each do |key, value|
-        if value.is_a? Hash # If the value is a nested parameter
-          filter_recursive(value, filter_param)
-        elsif key == filter_param.to_s # to_s is used to ensure that symbol/string difference does not matter
-          payload[key] = '[FILTERED]'
-        end
-      end
     end
   end
 end
