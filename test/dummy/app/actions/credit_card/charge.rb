b114793ee83d2339be256e2596790288630c0714
himn1 <hello@artofcode.co>
2024-06-24 22:53:12 +0200

    Refactoring

--- test/dummy/app/actions/credit_card/charge.rb
+++ test/dummy/app/actions/credit_card/charge.rb
@@ -7,7 +7,7 @@ class CreditCard
         create_res = create(payload.slice('user_id', 'credit_card'), opts.slice(:user))
         return create_res unless create_res.ok
 
-        charge(create_res.val[:credit_card].token, payload['amount'], create_res)
+        charge(create_res, payload['amount'])
       end
 
       private
@@ -19,7 +19,8 @@ class CreditCard
         Rdux::Result[ok: false, val: { errors: res.val[:errors] }, save: true, nested: [res]]
       end
 
-      def charge(token, amount, create_res)
+      def charge(create_res, amount)
+        token = create_res.val[:credit_card].token
         res = PaymentGateway.charge(token, amount)
         if res[:id].nil?
           Rdux::Result[ok: false, val: { errors: { base: 'Invalid credit card' } }, save: true,
