6641885de972a7542342bb4ac62be08028619c61
himn1 <hello@artofcode.co>
2025-06-08 22:08:59 +0200

    Rubocop

--- lib/rdux.rb
+++ lib/rdux.rb
@@ -79,7 +79,8 @@ module Rdux
     end
 
     def sanitize(action)
-      up_payload_sanitized = ActiveSupport::ParameterFilter.new(Rails.application.config.filter_parameters).filter(action.up_payload)
+      param_filter = ActiveSupport::ParameterFilter.new(Rails.application.config.filter_parameters)
+      up_payload_sanitized = param_filter.filter(action.up_payload)
       action.up_payload_sanitized = action.up_payload != up_payload_sanitized
       action.up_payload_unsanitized = action.up_payload if action.up_payload_sanitized
       action.up_payload = up_payload_sanitized
