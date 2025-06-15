9055a5ad38bb0b183165842aad34a74c9fe6f387
himn1 <hello@artofcode.co>
2025-06-15 06:25:43 +0200

    +1 test

--- test/models/rdux/action_test.rb
+++ test/models/rdux/action_test.rb
@@ -12,19 +12,17 @@ module Rdux
       assert_equal payload, create_task(user).action.payload
     end
 
-    describe '#up' do
-      it 'prevents performing a stored action if sanitized payload' do
-        skip 'implement after adding .store'
-        Rdux.dispatch(CreditCard::Create, TestData::Payloads.credit_card_create(users(:zbig)))
-      end
+    it 'prevents performing a stored action if sanitized payload' do
+      action = Rdux.store(CreditCard::Create, TestData::Payloads.credit_card_create(users(:zbig)))
+      assert_equal false, Action.find(action.id).call
+    end
 
-      it 'prevents performing an action again' do
-        user = users(:zbig)
-        res1 = Rdux.dispatch(Activity::Switch, { user_id: user.id, task_id: create_task(user).val[:task].id })
-        res2 = Rdux.dispatch(Activity::Stop, { user_id: user.id, activity_id: res1.val[:activity].id })
-        assert_equal false, res2.action.call
-        assert_equal false, res1.action.call
-      end
+    it 'prevents performing an action again' do
+      user = users(:zbig)
+      res1 = Rdux.dispatch(Activity::Switch, { user_id: user.id, task_id: create_task(user).val[:task].id })
+      res2 = Rdux.dispatch(Activity::Stop, { user_id: user.id, activity_id: res1.val[:activity].id })
+      assert_equal false, res2.action.call
+      assert_equal false, res1.action.call
     end
   end
 end
