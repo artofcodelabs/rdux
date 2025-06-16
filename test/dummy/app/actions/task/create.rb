9bc5789142755562534e9254c13d3c0eab2a696a
himn1 <hello@artofcode.co>
2025-06-16 05:59:18 +0200

    [test] refactoring

--- test/dummy/app/actions/task/create.rb
+++ test/dummy/app/actions/task/create.rb
@@ -3,8 +3,7 @@
 class Task
   class Create
     def call(payload, opts)
-      user = opts.dig(:ars, :user) || User.find(payload['user_id'])
-      task = user.tasks.new(payload['task'])
+      task = init_task(payload, opts)
       if task.save
         opts[:result] = { task_id: task.id }
         opts[:action].meta['inc'] += 1 if opts[:action].meta['inc']
@@ -12,6 +11,13 @@ class Task
       else
         Rdux::Result[false, { errors: task.errors }]
       end
+    end
+
+    private
+
+    def init_task(payload, opts)
+      user = opts.dig(:ars, :user) || User.find(payload['user_id'])
+      user.tasks.new(payload['task'])
     end
   end
 end
