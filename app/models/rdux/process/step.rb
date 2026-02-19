f18a140f57797132c3be24607f4dd697e0b65308
himn1 <hello@artofcode.co>
2026-02-19 06:01:44 +0100

    refactor

--- app/models/rdux/process/step.rb
+++ app/models/rdux/process/step.rb
@@ -3,19 +3,24 @@
 module Rdux
   class Process
     class Step
-      def initialize(step, process:)
-        @step = step
+      def initialize(action_performer, process:)
+        @action_performer = action_performer
         @process = process
       end
 
       def call(prev_res:, action_index:)
-        step_payload = payload_selector ? payload_selector.call(@step, payload, prev_res, action_index) : payload
-        if @step.is_a?(Proc)
-          @step.call(step_payload, @process)
+        step_payload = if payload_selector
+                         payload_selector.call(@action_performer, payload, prev_res,
+                                               action_index)
+                       else
+                         payload
+                       end
+        if @action_performer.is_a?(Proc)
+          @action_performer.call(step_payload, @process)
           return Rdux::Result[ok: nil]
         end
 
-        res = Rdux.perform(@step, step_payload, { process: @process })
+        res = Rdux.perform(@action_performer, step_payload, { process: @process })
         res.action.process = @process
         res.action.save!
         res
