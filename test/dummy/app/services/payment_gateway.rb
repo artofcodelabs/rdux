0ec31cf1734da96f9600fd4c71746929544ab19c
himn1 <hello@artofcode.co>
2026-03-03 05:31:14 +0100

    refactor

--- test/dummy/app/services/payment_gateway.rb
+++ test/dummy/app/services/payment_gateway.rb
@@ -1,21 +1,23 @@
 # frozen_string_literal: true
 
 module PaymentGateway
-  def self.tokenize(credit_card)
-    return false unless credit_card.number.match?(/\A\d{16}\z/)
+  class << self
+    def tokenize(credit_card)
+      return false unless credit_card.number.match?(/\A\d{16}\z/)
 
-    (0...4).map { rand(36).to_s(36) }.join + credit_card.number.to_s[-4..]
-  end
+      (0...4).map { rand(36).to_s(36) }.join + credit_card.number.to_s[-4..]
+    end
 
-  def self.delete_credit_card(token)
-    token
-  end
+    def delete_credit_card(token)
+      token
+    end
 
-  def self.charge(token, amount)
-    return false unless token.match?(/\A\w{8}\z/)
-    raise('Negative amount') if amount.negative?
+    def charge(token, amount)
+      return false unless token.match?(/\A\w{8}\z/)
+      raise('Negative amount') if amount.negative?
 
-    failure = (amount * 100).to_i == 9999
-    { id: failure ? nil : rand(1000), token:, amount: }
+      failure = (amount * 100).to_i == 9999
+      { id: failure ? nil : rand(1000), token:, amount: }
+    end
   end
 end
